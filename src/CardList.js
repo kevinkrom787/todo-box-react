5d6f3a66064ebe98829a9c8f5fab7ba4dbe7d93e
Kevin Krom <kevinkrom787@gmail.com>
2018-06-04 11:26:32 -0600

    Add a few more items

---
+++ src/CardList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const CardList = () => {
+
+  return(
+    <div>
+      <h3> title </h3>
+      <button> Delete Button </button>  
+      <h3> Body </h3> 
+      <button> upvote </button>
+      <button> down vote </button>
+      <p> quality: swill </p> 
+    </div> 
+  )
+}
+
+export default CardList;
